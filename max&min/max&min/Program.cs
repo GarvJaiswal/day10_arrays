9bdff3c23a1cd9aef047f5ec3a80e44047313424
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-24 16:13:26 +0530

    [Garv] Add:Added max & min qs

---
+++ max&min/max&min/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace max_min
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.MinValue;
+            int min=int.MaxValue;
+            int[] arr = { 1, 2, 3, 4, 5, 6, 100, 200, -99, 990 };
+            for(int i=0;i<arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+                if (arr[i] < min)   
+                    min = arr[i];
+            }
+            Console.WriteLine("Max is " + max);
+            Console.WriteLine("Min is " + min);
+            Console.ReadLine();
+        }
+    }
+}
