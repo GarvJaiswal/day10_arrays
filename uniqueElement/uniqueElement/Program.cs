d30ee64ab24eec3433771cdc4f897a615949c794
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-24 16:05:23 +0530

    [Garv] Add:Added unique element qs

---
+++ uniqueElement/uniqueElement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class unique
+{
+    public static void Main()
+    {
+        int[] arr = { 1, 2, 3, 3, 3, 2, 2, 1,1,2,3,9 };
+        int n = arr.Length;
+        for (int i = 0; i < n; i++)
+        {
+            int j;
+            for (j = 0; j < i; j++)
+                if (arr[i] == arr[j])
+                    break;
+
+            if (i == j)
+                Console.Write(arr[i] + " ");
+        }
+        Console.ReadLine();
+    }
+}
