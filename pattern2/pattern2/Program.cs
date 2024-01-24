7ac06f89e35d27651ce1076d306568e7aa9f29d2
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-24 16:14:18 +0530

    [Garv] Add:Added pattern2 qs

---
+++ pattern2/pattern2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        for (int i = 8; i >=1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                    Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
