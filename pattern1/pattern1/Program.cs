f265109ae9912091270b1a3ee733734a0d376d6e
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-24 16:10:34 +0530

    [Garv] Add:Added pattern1 qs

---
+++ pattern1/pattern1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 7; i++)
+        {
+            for (int j = 1; j <= 7; j++)
+            {
+                if (i == 1 || i == 7 || j == 1 || j == 7)
+                    Console.Write("*");
+                else
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
