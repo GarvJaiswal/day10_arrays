a742f503cf77255a61d7c335d5753e1d596a9e0a
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-24 16:07:57 +0530

    [Garv] Add:Added duplicate element qs

---
+++ duplicatenum/duplicatenum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace duplicatenum
+{
+    internal class Program
+    {
+        public void countDup(int[] nums)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                        count++;
+                    else continue;
+                }
+                Console.WriteLine($"Number {nums[i]} exists {count} times");
+            }
+        }
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Enter the size of array");
+            //nt n=Convert.ToInt32(Console.ReadLine());
+            int[] arr = {1, 1, 3, 4,4,5,6,6};
+           
+            Program obj = new Program();
+            obj.countDup(arr);
+            Console.ReadLine();
+
+        }
+    }
+}
